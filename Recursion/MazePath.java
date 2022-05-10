fe56d2c785625b0c03ae2fd62ab0a99d3205a445
Sahilbalhra <sahilbalhra0@gmail.com>
2022-05-10 21:59:05 +0530

    some change

--- Recursion/MazePath.java
+++ Recursion/MazePath.java
@@ -1,26 +1,49 @@
-import java.util.*;
-class MazePath{
-    public static void mazePath(int sr,int sc,int dr,int dc,String psf){
-        if(sr==dr && sc==dc){
-            System.out.println(psf);
-            return ;
-        }
-        //horizontal
-        if(sc+1<=dc){
-            mazePath(sr, sc+1, dr, dc, psf+"h");
-        }
-        //vertical
-        if(sr+1<=dr){
-            mazePath(sr+1, sc, dr, dc, psf+"v");
+class MazePath {
+    public static boolean inRange(int i, int j, int[][] arr) {
+        int n = arr.length;
+        int m = arr[0].length;
+
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return false;
+        return true;
+    }
+
+    public static boolean inRange(int i, int j, int n, int m) {
+        if (i < 0 || i >= n || j < 0 || j >= m)
+            return false;
+        return true;
+    }
+
+    public static int mazePath_01(int sr, int sc, int dr, int dc, int[][] dir, String[] dirN, String asf) {
+        if (sr == dr && sc == dc) {
+            System.out.println(asf);
+            return 1;
         }
 
+        int count = 0;
+        for (int d = 0; d < dir.length; d++) {
+            int r = sr + dir[d][0];
+            int c = sc + dir[d][1];
+
+            if (inRange(r, c, dr + 1, dc + 1)) {
+                count += mazePath_01(r, c, dr, dc, dir, dirN, asf + dirN[d]);
+            }
+        }
+        return count;
 
     }
+
+    public static void mazePath() {
+        int n = 3;
+        int m = 3;
+        String[] dirN = { "H", "V", "D" };
+        int[][] dir = { { 0, 1 }, { 1, 0 }, { 1, 1 } };
+
+        int ans = mazePath_01(0, 0, n - 1, m - 1, dir, dirN, "");
+        System.out.println(ans);
+    }
+
     public static void main(String[] args) {
-        Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
-        int m=scn.nextInt();
-        mazePath(0,0,n-1,m-1," ");
-        scn.close();
+        mazePath();
     }
 }
