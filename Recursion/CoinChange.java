5f1efbcfab3098d4e87ab5420f162243b73fb1e3
Sahilbalhra <sahilbalhra0@gmail.com>
2022-05-12 22:49:40 +0530

    Combination version added

--- Recursion/CoinChange.java
+++ Recursion/CoinChange.java
@@ -13,10 +13,26 @@ class CoinChange{
         }
         return count;
     }
+    //combination
+    public static int coinChangeCombInf(int [] coins, int tar,int idx, String asf){
+        if(tar == 0){
+            System.out.println(asf);
+            return 1;
+        }
+        int count = 0;
+        for(int i = idx; i<coins.length; i++){
+            if(tar - coins[i] >= 0){
+                count += coinChangeCombInf(coins, tar-coins[i],i, asf + coins[i]);
+            }
+        }
+        return count;
+    }
     public static void main(String[] args) {
         int []coins={2,3,5,7};
         int tar=10;
-        int ans =coinChangePermInf(coins,tar,"");
-        System.out.println(ans);
+        // int ans =coinChangePermInf(coins,tar,"");
+        // System.out.println("permutaion -->"+ans);
+        int ans =coinChangeCombInf(coins,tar,0,"");
+        System.out.println("Combination -->"+ans);
     }
 }
