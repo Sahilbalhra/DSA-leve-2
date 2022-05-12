7ac93d62f65708d2bc5bb5db9ece6591c887debb
Sahilbalhra <sahilbalhra0@gmail.com>
2022-05-12 23:45:04 +0530

    Single permutation added

--- Recursion/CoinChange.java
+++ Recursion/CoinChange.java
@@ -1,5 +1,5 @@
 class CoinChange{
-    //permutation case
+    //permutation infinate case
     public static int coinChangePermInf(int [] coins, int tar, String asf){
         if(tar == 0){
             System.out.println(asf);
@@ -13,8 +13,41 @@ class CoinChange{
         }
         return count;
     }
+    //permutation single case
+    public static int coinChangePermSingle(int [] coins, int tar, String asf){
+        if(tar == 0){
+            System.out.println(asf);
+            return 1;
+        }
+        int count = 0;
+        for(int i = 0; i<coins.length; i++){
+            if(coins[i]>0 &&tar-coins[i]>=0){
+                int val=coins[i];
+                // mark
+                coins[i]=-coins[i];
+                count += coinChangePermSingle(coins, tar-val, asf + val);
+                //ummark
+                coins[i]=-coins[i];
+            }
+        }
+        return count;
+    }
     //combination
     public static int coinChangeCombInf(int [] coins, int tar,int idx, String asf){
+        if(tar == 0){
+            System.out.println(asf);
+            return 1;
+        }
+        int count = 0;
+        for(int i = idx; i<coins.length; i++){
+            if(tar - coins[i] >= 0){
+                count += coinChangeCombInf(coins, tar-coins[i],i, asf + coins[i]);
+            }
+        }
+        return count;
+    }
+    //combination single 
+    public static int coinChangeCombSingle(int [] coins, int tar,int idx, String asf){
         if(tar == 0){
             System.out.println(asf);
             return 1;
