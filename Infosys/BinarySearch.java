5464f0b2378f231788c3c47d8e0f4eea40b5cede
Sahilbalhra <sahilbalhra0@gmail.com>
2022-07-18 22:50:36 +0530

    binary search

---
+++ Infosys/BinarySearch.java
@@ -0,0 +1,21 @@
+public class Solution {
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]==k){
+                return mid;
+            }
+            if(arr[mid]>k){
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+            
+        }
+        return -1;
+        
+    }
+} 
