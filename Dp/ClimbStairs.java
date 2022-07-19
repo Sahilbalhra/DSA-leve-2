e458b2fa8b56ed033ca34ff312b03ad338fc87dd
Sahilbalhra <sahilbalhra0@gmail.com>
2022-07-19 22:35:21 +0530

    dp started

---
+++ Dp/ClimbStairs.java
@@ -0,0 +1,58 @@
+// 1. You are given a number n, representing the number of stairs in a staircase.
+// 2. You are on the 0th step and are required to climb to the top.
+// 3. In one move, you are allowed to climb 1, 2 or 3 stairs.
+// 4. You are required to print the number of different paths via which you can climb to the top.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    // System.out.println(climb(n).size());
+    System.out.println(climb(n,new int[n+1]));
+    
+  }
+//   public static ArrayList<String> climb(int n) {
+//     if (n == 0) {
+//       ArrayList<String>base= new ArrayList<>();
+//       base.add("");
+//       return base;
+//     }
+//     ArrayList<String>ans= new ArrayList<>();
+//     for(int i=1;i<=3;i++){
+//         if(n-i>=0){
+//             ArrayList<String> rc=climb(n-i);
+//             for(String s:rc){
+//                 ans.add(i+s);
+//             }
+//         }
+//     }
+
+//     return ans;
+//   }
+
+public static int climb(int n,int []qb){
+    if(n==0){
+        return 1;
+    }else if(n<0){
+        return 0;
+    }
+    
+    if(qb[n]>0){
+        return qb[n];
+        
+    }
+    
+    int num1=climb(n-1,qb);
+     int num2=climb(n-2,qb);
+      int num3=climb(n-3,qb);
+      qb[n]=num1+num2+num3;
+      return num1+num2+num3;
+    
+}
+
+}
---
+++ Dp/Fibonacci.java
@@ -0,0 +1,50 @@
+// 1. You are given a number n.
+// 2. You are required to print the nth element of fibonnaci sequence.
+
+// Note -> Notice precisely how we have defined the fibonnaci sequence
+// 0th element -> 0
+// 1st element -> 1
+// 2nd element -> 1
+// 3rd element -> 2
+// 4th element -> 3
+// 5th element -> 5
+// 6th element -> 8
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    // write your code here
+    Scanner scn=new Scanner(System.in);
+    int n=scn.nextInt();
+    int ans=fibDp(n,new int[n+1]);
+    System.out.println(ans);
+    
+ }
+ public static int fib(int n){
+     if(n==0||n==1){
+         return n;
+     }
+     
+     int f1=fib(n-1);
+     int f2=fib(n-2);
+     return f1+f2;
+ }
+ public static int fibDp(int n,int []qb){
+     if(n==0||n==1){
+         return n;
+     }
+     if(qb[n]!=0){
+         return qb[n];
+     }
+     
+     int f1=fib(n-1);
+     int f2=fib(n-2);
+     qb[n]=f1+f2;
+     return f1+f2;
+ }
+}
