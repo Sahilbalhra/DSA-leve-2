ecf86d4310b666deda81a9ec990ccd1c77df4195
Sahilbalhra <sahilbalhra0@gmail.com>
2022-07-27 15:53:47 +0530

    lis added

---
+++ Infosys/LongestIncreasingSubsequence2.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+public class LongestIncreasingSubsequence2 {
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int[]arr=new int[n];
+        
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        
+        int dp[]=new int[n];
+        int omax=0;
+        
+        for(int i=0;i<n;i++){
+            int max=0;
+            
+            for(int j=0;j<i;j++){
+                if(arr[j]<arr[i]){
+                    if(dp[j]>max){
+                        max=dp[j];
+                    }
+                }
+            }
+            
+            dp[i]=max+1;
+            
+            if(dp[i]>omax){
+                omax=dp[i];
+            }
+        }
+        System.out.println(omax);
+        scn.close();
+    }
+    
+}
